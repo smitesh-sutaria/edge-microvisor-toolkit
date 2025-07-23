76d623545911c4251eb697d438c80f6c3bd392b7
Smitesh Sutaria <smitesh.sutaria@intel.com>
2025-07-23 08:12:11 +0530

    enhancements to take TAG as input
    Signed-off-by: Smitesh Sutaria <smitesh.sutaria@intel.com>

--- build_idv_iso.sh
+++ build_idv_iso.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
 GIT_REPO=https://github.com/open-edge-platform/edge-microvisor-toolkit.git
-TAG=3.0.20250718
+DEFAULT_TAG=3.0.20250718
 IDV_JSON=
 
+TAG=${1:-$DEFAULT_TAG}
+
 git clone $GIT_REPO
-#git checkout $TAG
 
 cd edge-microvisor-toolkit
 git checkout $TAG
+# pre-requisites
 sudo ./toolkit/docs/building/prerequisites-ubuntu.sh
 sudo ln -vsf /usr/lib/go-1.21/bin/go /usr/bin/go
 sudo ln -vsf /usr/lib/go-1.21/bin/gofmt /usr/bin/gofmt
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
-#git checkout $TAG
-cd toolkit
-wget https://raw.githubusercontent.com/smitesh-sutaria/edge-microvisor-toolkit/refs/heads/3.0/toolkit/imageconfigs/idv.json
-mv idv.json ./imageconfigs
 
-sudo make toolchain REBUILD_TOOLS=y VALIDATE_TOOLCHAIN_GPG=n --debug
+# build the toolkit
+cd toolkit
+# wget https://raw.githubusercontent.com/smitesh-sutaria/edge-microvisor-toolkit/refs/heads/3.0/toolkit/imageconfigs/idv.json
+cp ../../idv.json ./imageconfigs
+
+sudo make toolchain REBUILD_TOOLS=y VALIDATE_TOOLCHAIN_GPG=n
 sudo make iso -j8 REBUILD_TOOLS=y REBUILD_PACKAGES=n VALIDATE_TOOLCHAIN_GPG=n CONFIG_FILE=./imageconfigs/idv.json
 
+cp ../out/images/idv/*.iso ../../.
+
+cd ../../
+
+sudo rm -rf edge-microvisor-toolkit
 
